e9254c8e8acdab974dd40f0006a41312b09e83d7
Walid Abdullahi <109163621+Walidabdul@users.noreply.github.com>
2024-11-11 14:26:19 -0500

    Updated menu.php

---
+++ PHP_Game_Crafters/menu.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+// TODO: Add authentication check
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Main Menu</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="menu-container">
+        <h1>Main Menu</h1>
+        <a href="game.php">Start Game</a>
+        <a href="leaderboard.php">Leaderboard</a>
+        <!-- More menu items to be added -->
+    </div>
+</body>
+</html>
