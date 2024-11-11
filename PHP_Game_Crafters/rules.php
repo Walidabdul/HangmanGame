5e1c82b8b4f555aa36cb5c4ab248434da844029c
Walid Abdullahi <109163621+Walidabdul@users.noreply.github.com>
2024-11-11 14:29:44 -0500

    Updated rules.php

---
+++ PHP_Game_Crafters/rules.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Rules</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="rules-container">
+        <h2>Rules of the Game</h2>
+        <p>Explanation of the game rules goes here...</p>
+    </div>
+</body>
+</html>
