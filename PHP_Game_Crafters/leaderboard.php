93dae8482ee2c9d963cfa98c30f12fc7fe66537c
Walid Abdullahi <109163621+Walidabdul@users.noreply.github.com>
2024-11-11 14:28:22 -0500

    Updated leaderboard.php

---
+++ PHP_Game_Crafters/leaderboard.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Leaderboard</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="leaderboard-container">
+        <h2>Leaderboard</h2>
+        <!-- TODO: Implement leaderboard display -->
+        <a href="menu.php">Back to Menu</a>
+    </div>
+</body>
+</html>
