c05152edb64711a6fc59b4ac0536524930312eaf
Walid Abdullahi <109163621+Walidabdul@users.noreply.github.com>
2024-11-13 17:39:55 -0500

    Update menu.php

--- PHP_Game_Crafters/menu.php
+++ PHP_Game_Crafters/menu.php
@@ -1,19 +1,27 @@
 <?php
 session_start();
-// TODO: Add authentication check
+// Add authentication check
+if (!isset($_SESSION['logged_in'])) {
+    header("Location: login.php");
+    exit();
+}
 ?>
+    
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Main Menu</title>
+    <title class="titleGlow">Main Menu</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="menu-container">
-        <h1>Main Menu</h1>
-        <a href="game.php">Start Game</a>
-        <a href="leaderboard.php">Leaderboard</a>
-        <!-- More menu items to be added -->
+    <div class="container">
+        <h1 class="titleDrop">Main Menu</h1>
+        <a href="game.php" class="menu-link">Start Game</a>
+        <a href="leaderboard.php" class="menu-link">Leaderboard</a>
+        <a href="rules.php" class="menu-link">Rules</a>
+         <form method="post" action="logout.php">
+            <button type="submit">Logout</button>
+        </form>
     </div>
 </body>
 </html>
