16f4e70020ed19124b841b8c1db0b9846e06d0cb
Walid Abdullahi <109163621+Walidabdul@users.noreply.github.com>
2024-11-13 17:41:14 -0500

    Update rules.php

--- PHP_Game_Crafters/rules.php
+++ PHP_Game_Crafters/rules.php
@@ -5,9 +5,24 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="rules-container">
-        <h2>Rules of the Game</h2>
-        <p>Explanation of the game rules goes here...</p>
+   <div class="container">
+        <h2 class="titleGlow">Rules of the Game</h2>
+        <p>In Hangman, the goal is to guess the hidden word before the hangman figure is fully drawn. Here are the rules:</p>
+        <ul>
+            <li>Select a letter to guess one at a time.</li>
+            <li>If the letter is in the word, it will appear in the correct position(s).</li>
+            <li>If the letter is not in the word, a part of the hangman figure will be drawn.</li>
+            <li>You have a limited number of incorrect guesses before the game ends.</li>
+            <li>The game ends when either:</li>
+            <ul>
+                <li>You correctly guess the entire word.</li>
+                <li>The hangman figure is fully drawn, and you lose the round.</li>
+            </ul>
+            <li>You can select difficulty levels that affect the number of incorrect guesses allowed.</li>
+            <li>Try to guess the word with as few mistakes as possible for a higher score!</li>
+        </ul>
+		 <!-- Link to navigate back to the main menu -->
+        <a href="menu.php">Back to Menu</a>
     </div>
 </body>
 </html>
